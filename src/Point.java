711cfa6c2f02b3671603d35132a0f14f4a375fc2
baonguyen96 <baonguyendrump@gmail.com>
2017-10-29 23:00:07 -0500

    Add Point

---
+++ src/Point.java
@@ -0,0 +1,41 @@
+public class Point implements Comparable{
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Point anotherPoint = (Point) o;
+
+        if(this.x < anotherPoint.x) {
+            return -1;
+        }
+        else if(this.x == anotherPoint.x) {
+            return Integer.compare(this.y, anotherPoint.y);
+        }
+        else {
+            return 1;
+        }
+    }
+}
